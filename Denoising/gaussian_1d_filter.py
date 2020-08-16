334613c54c2a4d8ee00cfc81a39ca5b3a821190d
Aishwarya Jadhav <me.aishwaryajadhav@gmail.com>
2020-08-17 00:06:36 +0530

    Correction in name

--- Denoising/gaussian_1d_filter.py
+++ Denoising/gaussian_1d_filter.py
@@ -8,7 +8,7 @@ from pandas.tools.plotting import scatter matrix
 import matplotlib.pyplot as plt
 import scipy.io
 
-def build gaussian filter 1d(n,s,N):
+def build_gaussian_filter_1d(n,s,N):
     x = ( np.arange(n)-(n-1)/2 )/(N-1)
     f = np.exp( - x**2 /(2 * (s**2)) )
     f = f / f.sum()
@@ -22,7 +22,7 @@ A1 = dataset1['A'][7500:10001]
 B1 = dataset1['B'][7500:10001]
 print sta.signaltonoise(A1)
 print sta.signaltonoise(B1)
-f=build gaussian filter 1d(11,2,2501)
+f=build_gaussian_filter_1d(11,2,2501)
 s1=np.convolve( f[::-1], A1, mode='valid')
 s2=np.convolve( f[::-1], B1, mode='valid')
 comb=np.vstack((s1,s2)).T
